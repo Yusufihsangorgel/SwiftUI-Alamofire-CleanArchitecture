601b83a9df64ecbc6a3fdefe9981260500986f4d
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-12-30 10:28:01 +0300

    Initial Commit

---
+++ HttpDogsSwiftUI/HttpDogsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HttpDogsSwiftUIApp.swift
+//  HttpDogsSwiftUI
+//
+//  Created by Yusuf İhsan Görgel on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HttpDogsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
